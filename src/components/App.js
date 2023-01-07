46e84414a4ed67bd2145f7363c19f9572e282f20
Madysudh <madycomm2@gmail.com>
2023-01-07 18:04:17 +0530

    mm

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,80 @@
-import React, { useState } from "react";
-import '../styles/App.css';
-import Selection from './Selection';
-import ColourSelector from './ColourSelector';
+import React from "react";
+import "../styles/App.css";
 
-const colourConfig = [{
-    key: 'blue',
-    label: 'Blue',
-    classname: 'btn-blue',
-    background: 'rgb(34, 193, 195)'
-  }, {
-    key: 'orange',
-    label: 'Orange',
-    classname: 'btn-orange',
-    background: 'rgb(221, 112, 18)'
-  }, {
-    key: 'green',
-    label: 'Green',
-    classname: 'btn-green',
-    background: 'rgb(44, 209, 88)'
-  }
-]
+function App() {
 
-const title = 'Select the gradient and then the Box to change the color';
 
-const App = () => {
-  let [nextBackground, selectNextBackground] = useState({ background: "" })
-  const applyColor = (updateSelectionStyle) => {
-    updateSelectionStyle(nextBackground)
-  }
+const startDrawing = (e) => {
 
-  return (
-    <div id="master">
-      <h5 className="heading">{/* display title here */}</h5>
+};
 
-      <div className="row">
-        {colourConfig.map((config, index) => (
-          <ColourSelector key={config.key} config={config} selectNextBackground={selectNextBackground} />
-        ))}
-      </div>
 
-      <div className='row' id="children-wrapper">
-        {
-          ["selection1", "selection2", "selection3"].map(key => (
-            <Selection key={key} applyColor={applyColor} />
-          ))
-        }
-      </div>
-    </div >
-  )
+const endDrawing = () => {
+
+};
+
+const draw = (e) => {
+
+
+};
+
+return (
+	<div className="App">
+	<h1>Paint App</h1>
+	<div className="draw-area">
+		<Menu
+		
+		/>
+		<canvas
+		id="board"
+
+
+		width={`1280px`}
+		height={`720px`}
+		/>
+	</div>
+	</div>
+);
 }
 
 
-export default App;
+const Menu = ({  }) => {
+return (
+	<div className="Menu">
+	<label>Brush Color </label>
+	<input
+		type="color"
+		onChange={(e) => {
+
+		}}
+	/>
+	<label>Brush Width </label>
+	<input
+	    id="brush-width"
+		type="range"
+		min="3"
+		max="20"
+		onChange={(e) => {
+		}}
+	/>
+	<label>Brush Opacity</label>
+	<input
+		type="range"
+		min="1"
+		max="100"
+		onChange={(e) => {
+		
+		}}
+	/>
+	<label>Eraser</label>
+	<button
+		onClick={(e) => {
+	
+		}}
+	/>
+	</div>
+);
+};
+
+export default App ; 
+
